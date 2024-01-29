9f9c558f6059cf67ee9ba13391b4c46dbf6c7cd0
Karunakar Patel <karunakarpatel.kp@gmail.com>
2024-01-29 15:15:45 +0530

    Changed env file for testing the bug

--- next.config.js
+++ next.config.js
@@ -4,13 +4,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    PIXELS_API_KEY: process.env.PIXELS_API_KEY,
     HOME_PAGE_BASE_URL: process.env.HOME_PAGE_BASE_URL,
     ANALYTICS_URL: process.env.ANALYTICS_URL,
     ANALYTICS_ID: process.env.ANALYTICS_ID,
     ADSENSE_URL: process.env.ADSENSE_URL,
     HUGGING_FACE_TOKEN: process.env.HUGGING_FACE_TOKEN,
-    FACEBOOK_TEXT_TO_SPEECH_URL: process.env.FACEBOOK_TEXT_TO_SPEECH_URL,
     STABLE_DIFFUSION_2_1_URL: process.env.STABLE_DIFFUSION_2_1_URL,
     OPEN_JOURNEY_URL: process.env.OPEN_JOURNEY_URL,
     IP_ADAPTER_FACE_ID_URL: process.env.IP_ADAPTER_FACE_ID_URL,
